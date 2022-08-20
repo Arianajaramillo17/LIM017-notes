d2464507ef9f96d871c7ee34532c46b6dfe1f2f2
Arianajaramillo17 <arianajaramillo17@gmail.com>
2022-08-19 19:46:09 -0500

    Modal Edit semi-term

--- cat-notes/src/components/Edit.js
+++ cat-notes/src/components/Edit.js
@@ -1,147 +1,66 @@
-
-import React, { useState, useEffect } from "react";
-import {
-  collection,
-  onSnapshot,
-  where,
-  query,
-  getDoc,
-  doc,
-  addDoc
-} from 'firebase/firestore';
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+import { getDoc, doc } from 'firebase/firestore';
 import { db, updateNote } from '../firebase';
+import { async } from "@firebase/util";
+export function Edit() {
 
 
-export function Edit(props) {
-  const inicializeDataInputs = {
-    title: "",
-    post: "",
-    author: localStorage.getItem("email"),
-  };
-
-  const [dataInputs, setDataInputs] = useState(inicializeDataInputs);
-  const [currentId, setCurrentId] = useState('');
-  const [notes, setNotes] = useState([]);
- 
-
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setDataInputs({ ...dataInputs, [name]: value });
-    console.log(name, value)
-  };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-   addnote(dataInputs);
-    setDataInputs({ ...inicializeDataInputs });
-  };
-  const getNoteById = async (id) => {
-    const docRefId = doc(db, "notes", id);
-    const docSnap = await getDoc(docRefId);
-
-    if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
-    } else {
-      console.log("No such document!");
-    }
-    setDataInputs({ ...docSnap.data() })
-  }
-
-  const addnote = async (objectNote) => {
-    console.log(currentId);
-    if (currentId === '') {
-      const docRef = await addDoc(collection(db, "notes"), objectNote);
-      console.log("Document written with ID: ", docRef.id);
-    } else {
-      await updateNote(currentId, objectNote.title, objectNote.post).then(() => {
-        getNotes();
-      })
-    }
-  };
-
-
-  const getNotes = async () => {
-    const q = query(
-      collection(db, "notes"),
-      where("author", "==", localStorage.getItem("email"))
-    );
-    onSnapshot(q, (querySnapshot) => {
-      const docs = [];
-      querySnapshot.forEach((doc) => {
-        docs.push({ ...doc.data(), id: doc.id });
-      });
-      setNotes(docs);
-    });
-  };
-  getNotes();
-
-
-  useEffect(() => {
     const inicializeDataInputs = {
-      title: "",
-      post: "",
-      author: localStorage.getItem("email"),
-    };
-    if (currentId === '') {
-      setDataInputs({ ...inicializeDataInputs });
-    } else {
-      getNoteById(currentId)
-    }
-    getNotes()
-  }, [currentId]);
+        title: "",
+        post: "",
+        author: localStorage.getItem("email"),
+      };
 
-  return (
-    <section className='containerHome'>
-      <div className='logoHome'>
-      </div>
-      <form className='containerNotes' onSubmit={handleSubmit}>
-        <div className='containerPost'>
-          <label className='imputLitle'>
-            <input type='text' name="title" placeholder="Titulo" onChange={handleInputChange} value={dataInputs.title} />
-          </label>
-          <label className='imputPost'>
-            <textarea type='text' name="post" placeholder="Escribe una nota" onChange={handleInputChange} value={dataInputs.post}>
-            </textarea>
-          </label>
-          <button className="btnPrimary" >guardar</button>
-        </div>
-      </form>
+    const [dataInputs, setDataInputs] = useState(inicializeDataInputs);
+    const [currentId, setCurrentId] = useState('');
+    const [notes, setNotes] = useState([]);
+    const navigate= useNavigate();
+    const {id}=useParams
 
-      <section>
-        <div className="notesList">
-          {(
-            <div className="notesContent" key={props.note.id} id={props.note.id}>
-              <div className="noteCard">
-                <div className="contentBtnEdit">
-                </div>
-                <div className="contentBtnClose">
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setDataInputs({ ...dataInputs, [name]: value });
+        console.log(name, value)
+      };
+      const handleSubmit = (e) => {
+        e.preventDefault();
+    update(dataInputs);
+        setDataInputs({ ...inicializeDataInputs });
+      };
+const update= async(objectNote)=>{
+         objectNote.preventDefault() 
+         const note= doc(db,"notes", id)
+         //const data={title: title, post:post}
+           await updateNote(currentId, objectNote.title, objectNote.post)
+        navigate('/')
+       }
+const getNoteById= async(id)=> {
+    const note=await getDoc(doc(db,"notes", id))
+    if(note.exists()){
+        setNotes(notes.data().title)
+        setNotes(notes.data().post)
+    }else{
+        console.log('Nota no existe')
+    }}
+    useEffect(() => {
+        const inicializeDataInputs = {
+          title: "",
+          post: "",
+          author: localStorage.getItem("email"),
+        };
+        if (currentId === '') {
+          setDataInputs({ ...inicializeDataInputs });
+        } else {
+          getNoteById(currentId)
+        }
+      }, [currentId]);
 
-                </div>
-                <input
-                  //disabled={editNoteSelected !== index}
-                  className="editTitleLoad"
-                  value={props.note.title}
-                  onChange={handleInputChange}
-                />
-                <textarea
-                  //disabled={editNoteSelected !== index}
-                  className="editDescriptionLoad"
-                  rows="5"
-                  value={props.note.post}
-                  onChange={handleInputChange}
-                >
-                </textarea>
-              </div>
-            </div>
-          )}
-        </div>
+    return (
+        <section className='containerHome'> hola
+            </section>
 
-      </section>
-
-
-
-    </section>
-  );
+        );
 
 }
 export default Edit;
